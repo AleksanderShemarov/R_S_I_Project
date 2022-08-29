f129c21229bb3a04cd8110f728616509b4d0b5b9
AlexanderShemarov <shemarov9822@gmail.com>
2022-08-29 17:02:42 +0300

    At the night I didn't see that the project doesn't work at 'localhost'. I have changed it adding '127.0.0.1'. And in the end: I created administrator to the project in a PostgreSQL database in a shell ('py manage.py shell' command).

--- fundament/settings.py
+++ fundament/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-krvm9831*6c78@9n&t#u4_iucfq9+r+x_ygkaqb5rhd#n10klx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
