b6481acc3b65a5ffbf97d885239c9a072bba374e
AlexanderShemarov <shemarov9822@gmail.com>
2022-09-04 01:26:01 +0300

    For the future I try to create my own User form from the AbstractUser. Now all in it look like comments and unworkable.

---
+++ baseInfo/forms.py
@@ -0,0 +1,17 @@
+# from django import forms
+from django.contrib.auth.models import User
+# from .models import User
+#
+#
+# class Registration(forms.Form):
+#
+#     username = forms.CharField(required=False, label="Your nickname, if you want")
+#     name = forms.CharField(label="Your name")
+#     surname = forms.CharField(label="Your surname")
+#     email = forms.EmailField(label="post@org.com")
+#
+#     class Meta:
+#
+#         model = User
+#         fields = "__all__"  # ["name", "surname", "surname", "username"]
+
